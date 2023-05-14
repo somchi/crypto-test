0d9885f91cb08a714c113ea5b51eea1eec4ce8b4
somchi <chisomike11@gmail.com>
2023-05-14 16:52:24 +0100

    changed url

--- src/site-settings/site-navigation.ts
+++ src/site-settings/site-navigation.ts
@@ -1,9 +1,4 @@
 export const ASSET = {
   name: 'Asset',
-  href: '/markets/[asset]',
-};
-
-export const ASSET_PAIR = {
-  name: 'Asset Pair',
-  href: '/trade/[assetPair]',
+  href: '/trade/[asset]',
 };
